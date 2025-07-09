bb585cc191184fcb7f33d68ed9a36428aa4f5681
devdral <84059355+devdral@users.noreply.github.com>
2025-07-08 18:06:06 -0700

    Add AST classes for the AST nodes of VeryBasic

---
+++ VeryBasic.Runtime/Ast.cs
@@ -0,0 +1,89 @@
+using System.Security.AccessControl;
+
+namespace VeryBasic.Runtime;
+
+
+
+public interface INode {}
+
+public class ProcCallNode : INode
+{
+    public string Name;
+    public List<IExpressionNode> Args;
+}
+
+public interface IExpressionNode : INode {}
+
+public class TheResultNode : IExpressionNode {}
+
+public class ValueNode : IExpressionNode
+{
+    private Value _value;
+}
+
+public enum BinOp
+{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    And,
+    Or,
+    Equal,
+    NotEqual
+}
+
+public class BinaryOpNode : IExpressionNode
+{
+    public IExpressionNode Left;
+    public IExpressionNode Right;
+    public BinOp Op;
+}
+
+public enum UnaryOp
+{
+    Negate,
+    Invert
+}
+
+public class UnaryOpNode : IExpressionNode
+{
+    public IExpressionNode Expr;
+    public UnaryOp Op;
+}
+
+public class IfNode : INode
+{
+    public IExpressionNode Condition;
+    public List<INode> Then;
+    public List<INode>? Else;
+}
+
+public class VarDecNode : INode
+{
+    public string Name;
+    public IExpressionNode? Value;
+}
+
+public class VarSetNode : INode
+{
+    public string Name;
+    public IExpressionNode Value;
+}
+
+public class CalculateNode : INode
+{
+    public IExpressionNode Expr;
+}
+
+public class RepeatLoopNode : INode
+{
+    public IExpressionNode Times;
+    public List<INode> Loop;
+}
+
+public class WhileLoopNode : INode
+{
+    public IExpressionNode Condition;
+    public List<INode> Loop;
+}
