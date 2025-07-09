d0aa383194d65ce5f78de77963ab1b471cbf4167
devdral <84059355+devdral@users.noreply.github.com>
2025-07-08 18:05:16 -0700

    Add type to represent values in VeryBasic

---
+++ VeryBasic.Runtime/Value.cs
@@ -0,0 +1,40 @@
+namespace VeryBasic.Runtime;
+
+public class Value
+{
+    public VBType Type;
+    private object _value;
+
+    public T Get<T>()
+    {
+        return (T)_value;
+    }
+
+    public Value(object value)
+    {
+        Type type = value.GetType();
+        if (type == typeof(double))
+        {
+            this.Type = VBType.Number;
+        } else if (type == typeof(bool))
+        {
+            this.Type = VBType.Boolean;
+        } else if (type == typeof(string))
+        {
+            this.Type = VBType.String;
+        }
+        else
+        {
+            throw new InvalidCastException();
+        }
+
+        _value = value;
+    }
+}
+
+public enum VBType
+{
+    Number,
+    String,
+    Boolean,
+}
