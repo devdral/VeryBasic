13fc6d58af6a738937efb6ad3bf731662ab73a26
Devon Albert <devon.drake.albert@outlook.com>
2025-07-21 15:34:53 -0700

    Add Visit methods for new nodes

--- VeryBasic.Runtime/Parsing/IVisitor.cs
+++ VeryBasic.Runtime/Parsing/IVisitor.cs
@@ -6,4 +6,9 @@ public interface IVisitor<out T>
     public T VisitUnaryOpNode(UnaryOpNode node);
     public T VisitBinaryOpNode(BinaryOpNode node);
     public T VisitTheResultNode(TheResultNode node);
+    public T VisitVarDecNode(VarDecNode node);
+    public T VisitVarSetNode(VarSetNode node);
+    public T VisitProcCallNode(ProcCallNode node);
+    public T VisitVarRefNode(VarRefNode node);
+    public T VisitIfNode(IfNode node);
 }
